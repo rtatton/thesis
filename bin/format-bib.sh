43bdf3d1068fe01ab4e8d54c2661979e5d6fcd2f
rtatton <rdt17@proton.me>
2024-02-17 16:22:43 -0500

    Checkpoint

---
+++ bin/format-bib.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Ref: https://github.com/FlamingTempura/bibtex-tidy
+# Ref: https://flamingtempura.github.io/bibtex-tidy/manual/key-generation.html
+
+bibtex-tidy $1 \
+--blank-lines \
+--curly \
+--duplicates \
+--encode-urls \
+--modify \
+--no-align \
+--no-merge \
+--no-wrap \
+--numeric \
+--omit=abstract,address,isbn,issn,issue_date,keywords,location,month,numpages \
+--remove-dupe-fields \
+--remove-empty-fields \
+--sort-fields \
+--sort=year,name \
+--space=2 \
+--strip-comments \
+--strip-enclosing-braces \
+--v2
